56cfbfbcd4b113d4475557d85b11cc6449cd7ae9
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2026-03-03 15:10:57 +0000

    🚧

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,42 +1,42 @@
 "use client";
 
-import { MascotIcon } from "../(main)/cpn/MascotIcon";
+import { BlockGroup } from "./BlockGroup";
 
 export default function TestPage() {
-	return (
-		<div className="w-screen h-screen flex justify-center items-center">
-			<MascotIcon height={150} />
-		</div>
-	);
 	// return (
-	// 	<BlockGroup
-	// 		blocks={[
-	// 			{ id: "attack", color: "red", items: [{ type: "text", text: "Attack" }] },
-	// 			{
-	// 				id: "turn-r",
-	// 				color: "purple",
-	// 				items: [{ type: "text", text: "Turn right" }]
-	// 			},
-	// 			{
-	// 				id: "cheer",
-	// 				color: "yellow",
-	// 				items: [{ type: "text", text: "Cheer" }]
-	// 			},
-	// 			{
-	// 				id: "walk",
-	// 				color: "blue",
-	// 				items: [
-	// 					{ type: "text", text: "Walk" },
-	// 					{ type: "number-input" },
-	// 					{ type: "text", text: "pixels" }
-	// 				]
-	// 			},
-	// 			{
-	// 				id: "grass",
-	// 				color: "green",
-	// 				items: [{ type: "text", text: "Touch grass" }]
-	// 			}
-	// 		]}
-	// 	/>
+	// 	<div className="w-screen h-screen flex justify-center items-center">
+	// 		<MascotIcon height={150} />
+	// 	</div>
 	// );
+	return (
+		<BlockGroup
+			blocks={[
+				{ id: "attack", color: "red", items: [{ type: "text", text: "Attack" }] },
+				{
+					id: "turn-r",
+					color: "purple",
+					items: [{ type: "text", text: "Turn right" }]
+				},
+				{
+					id: "cheer",
+					color: "yellow",
+					items: [{ type: "text", text: "Cheer" }]
+				},
+				{
+					id: "walk",
+					color: "blue",
+					items: [
+						{ type: "text", text: "Walk" },
+						{ type: "number-input" },
+						{ type: "text", text: "pixels" }
+					]
+				},
+				{
+					id: "grass",
+					color: "green",
+					items: [{ type: "text", text: "Touch grass" }]
+				}
+			]}
+		/>
+	);
 }
